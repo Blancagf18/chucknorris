11f2adf21311ec82d626357979c13b211f33aa93
Blanca <blancagarciaflores18@gmail.com>
2025-05-06 13:29:53 +0200

    httparty and basic logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    uri = URI("https://api.chucknorris.io/jokes/random")
+    response = Net::HTTP.get(uri)
+    @joke = JSON.parse(response)["value"]
   end
 end
