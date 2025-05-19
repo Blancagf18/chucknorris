fcf01e9e062f057bf99fa9e24a9617fee37d0e7b
Blanca <blancagarciaflores18@gmail.com>
2025-05-19 21:17:29 +0200

    locales configured with i18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Chucknorris
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config/application.rb
+config.i18n.available_locales = [:en, :es]
+config.i18n.default_locale = :es
+
   end
 end
